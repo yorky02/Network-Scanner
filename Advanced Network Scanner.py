1e3a87761727389d583edbbd4bd184e0d4e8055d
yorky02 <vieiraoctavio_@outlook.com>
2025-12-29 09:33:35 -0800

    removed line 18, which was a call for is_port_open function. add method for user to select IP address and select if want to scan ports from 1 to 1025 or scan single given port. built a menu that let's you select if you want to scan one single port or multiple ports. Also, if you select option 2 (scan multiple ports) it let you choose between list of ports separated by commas, or a range (ex: 80 - 900). This method still in progress.

--- Advanced Network Scanner.py
+++ Advanced Network Scanner.py
@@ -1,10 +1,16 @@
 import socket
 
+
 # network scanner
+
+def welcome():
+    print('Welcome to the Advanced Network Scanner!')
+    print("By Octavio Vieira")
+    print()
 
 def is_port_open(ip, port):
     port_checker = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    port_checker.settimeout(3)
+    port_checker.settimeout(0.5)
     condition_helper = port_checker.connect_ex((ip, port))
 
     try:
@@ -15,13 +21,48 @@ def is_port_open(ip, port):
     finally:
         port_checker.close()
 
-is_port_open('localhost', 7000)
+def ask_user():
+    ip = input('Please enter ip address or hostname: ')
+    print(ip)
+    print()
+    print('Select one of the following options: 1) Scan single port or 2) Scan multiple ports.')
+    while True:
+        try:
+            port_scanning_type = int(input('Please enter port scanning type (number): '))
+            port = int(input("What port would you like to scan?: "))
+            if port_scanning_type == 1:
+                print(port)
+            elif is_port_open(ip, port):
+                print("Port is open")
+            else:
+                print("Port is closed")
+        except ValueError:
+                print("Please enter a valid port. It must be an integer.")
 
-ports = []
+    #single_or_multiple_ports = input('Would you like to scan for multiple ports (y/n): ')
+    #if single_or_multiple_ports.lower() == 'y':
+        #print('Scanning for open ports...')
 
-for port in range(1, 7001):
-    if is_port_open('localhost', port):
-        ports.append(port)
-print('The following ports are open:', ports)
+        #ports = []
+
+        #for port in range(1, 1025):
+            #if is_port_open(ip, port):
+                #ports.append(port)
+        #print('Open ports:', ports)
+
+    #elif single_or_multiple_ports.lower() == 'n':
+
+        #try:
+            #port = int(input("What port would you like to scan?: "))
+            #if is_port_open(ip, port):
+                #print("Port is open")
+            #else:
+                #print("Port is closed")
+        #except ValueError:
+            #print("Please enter a valid port. It must be an integer.")
+            #ask_user()
+
+welcome()
+ask_user()
 
 
