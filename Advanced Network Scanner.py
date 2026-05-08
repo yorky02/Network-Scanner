d6f41e83d99375dfd3b0719998e43ca3d8453179
yorky02 <vieiraoctavio_@outlook.com>
2026-05-07 18:14:22 -0700

    re-organized the code. created a def for each type of task i need the program to perform. also, added a mini menu that asks what type of scan the user would like to realize.
    -fixed bugs
    -correct code logic*

--- Advanced Network Scanner.py
+++ Advanced Network Scanner.py
@@ -7,6 +7,7 @@ def welcome():
     print('Welcome to the Advanced Network Scanner!')
     print("By Octavio Vieira")
     print()
+welcome()
 
 def is_port_open(ip, port):
     port_checker = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -21,48 +22,44 @@ def is_port_open(ip, port):
     finally:
         port_checker.close()
 
-def ask_user():
-    ip = input('Please enter ip address or hostname: ')
-    print(ip)
+def single_port_scanner(ip, port):
+    is_port_open(ip, port)
+    try:
+        if is_port_open(ip, port):
+            print("Port is open")
+        else:
+            print("Port is closed")
+    finally:
+        print()
+        print("thank you for using this tool !!!")
+
+ip = input('Please enter ip address or hostname: ')
+options = int(input('Select one of the following options: 1) Scan single port or 2) Scan multiple ports.'))
+
+if options == 1:
     print()
-    print('Select one of the following options: 1) Scan single port or 2) Scan multiple ports.')
-    while True:
-        try:
-            port_scanning_type = int(input('Please enter port scanning type (number): '))
-            port = int(input("What port would you like to scan?: "))
-            if port_scanning_type == 1:
-                print(port)
-            elif is_port_open(ip, port):
-                print("Port is open")
-            else:
-                print("Port is closed")
-        except ValueError:
-                print("Please enter a valid port. It must be an integer.")
+    port = int(input("What port would you like to scan?: "))
+    print()
+    single_port_scanner(ip, port)
+elif options == 2:
+    print()
+    print('This function is getting updated. Sorry!')
+    print()
+elif options != 1 and options != 2:
+    print()
+    print("Please enter a valid port. It must be an integer.")
+    print()
 
-    #single_or_multiple_ports = input('Would you like to scan for multiple ports (y/n): ')
-    #if single_or_multiple_ports.lower() == 'y':
-        #print('Scanning for open ports...')
+#port = int(input("What port would you like to scan?: "))
 
-        #ports = []
+#try:
+    #port_scanning_type = int(input('Please enter port scanning type (number): '))
+    #if port_scanning_type == port_scanning_type:
+       # print(single_port_scanner(ip, port))
+#except ValueError:
+        #print("Please enter a valid port. It must be an integer.")
 
-        #for port in range(1, 1025):
-            #if is_port_open(ip, port):
-                #ports.append(port)
-        #print('Open ports:', ports)
 
-    #elif single_or_multiple_ports.lower() == 'n':
 
-        #try:
-            #port = int(input("What port would you like to scan?: "))
-            #if is_port_open(ip, port):
-                #print("Port is open")
-            #else:
-                #print("Port is closed")
-        #except ValueError:
-            #print("Please enter a valid port. It must be an integer.")
-            #ask_user()
-
-welcome()
-ask_user()
 
 
